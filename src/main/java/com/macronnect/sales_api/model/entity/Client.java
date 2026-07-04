9129120e38d519603d27816a94118cd76b35e7ee
Adrieliyo <AngelRodriguez2003@Outlook.com>
2026-07-04 00:55:48 -0700

    Database configuration in application.properties

--- src/main/java/com/macronnect/sales_api/model/entity/Client.java
+++ src/main/java/com/macronnect/sales_api/model/entity/Client.java
@@ -1,4 +1,69 @@
 package com.macronnect.sales_api.model.entity;
+import javax.persistence.*;
 
+@Entity
+@Table(name = "clients")
 public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private int phone;
+
+    private String address;
+
+    public Client() {}
+
+    public Client(Long id, String name, String email, int phone, String address) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
